9a3fcc94dc7fb1562870b31eb39ce7f927316630
Hanze Dong <hendrydong@icloud.com>
2025-04-20 13:58:07 +0800

    Update __init__.py

--- verl/utils/reward_score/__init__.py
+++ verl/utils/reward_score/__init__.py
@@ -18,16 +18,16 @@ def _default_compute_score(data_source, solution_str, ground_truth, extra_info=N
     if data_source == 'openai/gsm8k':
         from . import gsm8k
         res = gsm8k.compute_score(solution_str, ground_truth)
-    elif data_source in ['lighteval/MATH', 'DigitalLearningGmbH/MATH-lighteval']:
-        from . import math
-        res = math.compute_score(solution_str, ground_truth)
+    elif data_source in ['lighteval/MATH', 'DigitalLearningGmbH/MATH-lighteval', "numina_math"]:
+        #from . import math
+        #res = math.compute_score(solution_str, ground_truth)
         # [Optional] Math-Verify Integration
         # For enhanced accuracy, consider utilizing Math-Verify (https://github.com/huggingface/Math-Verify).
         # Note: Math-Verify needs to be manually installed via pip: `pip install math-verify`.
         # To use it, override the `compute_score` function with the following implementation:
 
-        # from . import math_verify
-        # res = math_verify.compute_score(solution_str, ground_truth)
+        from . import math_verify
+        res = math_verify.compute_score(solution_str, ground_truth)
     elif data_source == 'math_dapo' or data_source.startswith("aime"):
         from . import math_dapo
         res = math_dapo.compute_score(solution_str, ground_truth)
